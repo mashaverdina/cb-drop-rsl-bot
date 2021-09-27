458adeb2086252393517d8f7514ccec472d2d17e
Mariia Verdina <masha.verdina@gmail.com>
2021-09-28 01:09:42 +0300

    Add choose month and stats keyboards

--- pkg/keyboards/keyboards.go
+++ pkg/keyboards/keyboards.go
@@ -5,17 +5,34 @@ import (
 )
 
 const (
-	Cb5          = "😈 Добавить дроп с 5 КБ"
-	Cb6          = "👹 Добавить дроп с 6 КБ"
-	Stats        = "📈 Посмотреть статистику"
-	Approve      = "✅ OK"
-	Reject       = "❌ Закрыть"
-	Clear        = "🔄 Ввести заново"
-	AncientShard = "💙 Древний осколок"
-	VoidShard    = "💜 Темный осколок"
-	SacredShard  = "💛 Сакральный осколок"
-	EpicTome     = "📘 Эпический том"
-	LegTome      = "📙 Легендарный том"
+	Cb5             = "😈 Добавить дроп с 5 КБ"
+	Cb6             = "👹 Добавить дроп с 6 КБ"
+	Stats           = "📈 Посмотреть статистику"
+	Approve         = "✅ OK"
+	Reject          = "❌ Закрыть"
+	Clear           = "🔄 Ввести заново"
+	AncientShard    = "💙 Древний осколок"
+	VoidShard       = "💜 Темный осколок"
+	SacredShard     = "💛 Сакральный осколок"
+	EpicTome        = "📘 Эпический том"
+	LegTome         = "📙 Легендарный том"
+	LastVoidShard   = "💜 Последний темный осколок"
+	LastSacredShard = "💛 Последний сакральный осколок"
+	LastLegTome     = "📙 Последний легендарный том"
+	MonthStats      = "📅 Статистика за месяц"
+	Back            = "🔙 Назад"
+	Jan             = "Январь"
+	Feb             = "Февраль"
+	Mar             = "Март"
+	Apr             = "Апрель"
+	May             = "Май"
+	Jun             = "Июнь"
+	Jul             = "Июль"
+	Aug             = "Август"
+	Sep             = "Сентябрь"
+	Oct             = "Октябрь"
+	Nov             = "Ноябрь"
+	Dec             = "Декабрь"
 )
 
 var HelloKeyboard = tgbotapi.NewReplyKeyboard(
@@ -40,5 +57,40 @@ var NumericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
 	tgbotapi.NewInlineKeyboardRow(
 		tgbotapi.NewInlineKeyboardButtonData(Approve, Approve),
 		tgbotapi.NewInlineKeyboardButtonData(Reject, Reject),
+	),
+)
+
+var StatsKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(Back),
+		tgbotapi.NewKeyboardButton(MonthStats),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(LastVoidShard),
+		tgbotapi.NewKeyboardButton(LastSacredShard),
+		tgbotapi.NewKeyboardButton(LastLegTome),
+	),
+)
+var ChooseMonthKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(Jan),
+		tgbotapi.NewKeyboardButton(Feb),
+		tgbotapi.NewKeyboardButton(Mar),
+		tgbotapi.NewKeyboardButton(Apr),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(May),
+		tgbotapi.NewKeyboardButton(Jun),
+		tgbotapi.NewKeyboardButton(Jul),
+		tgbotapi.NewKeyboardButton(Aug),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(Sep),
+		tgbotapi.NewKeyboardButton(Oct),
+		tgbotapi.NewKeyboardButton(Nov),
+		tgbotapi.NewKeyboardButton(Dec),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(Back),
 	),
 )
